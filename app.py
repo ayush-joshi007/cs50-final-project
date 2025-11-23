323186dad3c79c204623e7f6643d67aa725b29e2
ayush-joshi007 <joshiayush1975@gmail.com>
2025-11-23 19:54:03 +0530

    worked on navbar

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, redirect, session
+from flask_session import Session
+import sqlite3
+
+app = Flask(__name__)                          #Start the web app
+app.config["SESSION_PERMANENT"] = False        #Don’t keep user logged in forever
+app.config["SESSION_TYPE"] = "filesystem"      #Store login sessions on the server
+Session(app)                                   #Activate the session system
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
